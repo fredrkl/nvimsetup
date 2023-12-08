1f5db22ad8293997a0675a2d027ec9880c3c9168
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-08 22:38:52 +0100

    feat: telescope
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/packer.lua
+++ lua/fredrkl/packer.lua
@@ -6,5 +6,9 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	  -- or                            , branch = '0.1.x',
+         requires = { {'nvim-lua/plenary.nvim'} }
+  }
 end)
