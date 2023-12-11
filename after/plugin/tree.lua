9328a16565c0c9c604d0a695378befb13145734e
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-11 20:45:42 +0100

    feat: neovim tree setup
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

---
+++ after/plugin/tree.lua
@@ -0,0 +1,25 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle<CR>", {noremap = true, silent = true})
+
