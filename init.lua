1e14aab74a609889955b3b16b4dfb0d245559b0b
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-15 08:30:21 +0100

    chore: cleanup from packer
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-require("fredrkl")
 vim.opt.eol = true
 vim.wo.relativenumber = true
 vim.wo.number = true
+require("fredrkl")
