510df617ae9429e29d9d75f0f4602058091d674b
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-16 11:33:55 +0100

    feat: telescope plugin
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/packer.lua
+++ lua/fredrkl/packer.lua
@@ -7,9 +7,6 @@
 --  -- Packer can manage itself
 --  use 'wbthomason/packer.nvim'
 --  use {
---	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
---	  -- or                            , branch = '0.1.x',
---         requires = { {'nvim-lua/plenary.nvim'} }
 --  }
 --  use 'mbbill/undotree'
 --end)
