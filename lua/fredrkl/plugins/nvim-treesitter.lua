f791ab8caf861c61f66f2340e65f05c1831636b9
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-17 15:53:49 +0100

    feat: Install all parser for treesitter
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/plugins/nvim-treesitter.lua
+++ lua/fredrkl/plugins/nvim-treesitter.lua
@@ -8,7 +8,7 @@ return {
   build = ':TSUpdate',
   config = function()
     require'nvim-treesitter.configs'.setup {
-      ensure_installed = 'bicep',
+      ensure_installed = "all",
       highlight = {
         enable = true,
       },
