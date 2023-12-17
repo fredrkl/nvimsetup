b4a24bea0c2cf4b179f996a14fd0fd64c09f4d39
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-17 22:37:57 +0100

    feat: need terraform as well for treesitter
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/plugins/nvim-treesitter.lua
+++ lua/fredrkl/plugins/nvim-treesitter.lua
@@ -8,7 +8,7 @@ return {
   build = ':TSUpdate',
   config = function()
     require'nvim-treesitter.configs'.setup {
-      ensure_installed = 'bicep',
+      ensure_installed = {"bicep", "terraform"}
       highlight = {
         enable = true,
       },
