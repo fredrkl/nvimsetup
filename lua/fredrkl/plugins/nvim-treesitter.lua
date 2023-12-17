4a057f74bd33fa198a45ef3886583a8f85a59d28
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-17 13:13:14 +0100

    closes: 11
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

---
+++ lua/fredrkl/plugins/nvim-treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  -- Highlight, edit, and navigate code
+  'nvim-treesitter/nvim-treesitter',
+  event = "BufRead",
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  build = ':TSUpdate',
+  config = function()
+    require'nvim-treesitter.configs'.setup {
+      ensure_installed = 'bicep',
+      highlight = {
+        enable = true,
+      },
+    }
+  end,
+}
