480fff3a8ee0007f6411c2274abcc042304cbe87
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-17 22:56:05 +0100

    feat: more space to explorer
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/plugins/nvim-tree.lua
+++ lua/fredrkl/plugins/nvim-tree.lua
@@ -15,7 +15,7 @@ return {
     -- configure nvim-tree
     nvimtree.setup({
       view = {
-        width = 35,
+        width = 45,
         relativenumber = true,
       },
       -- change folder arrow icons
