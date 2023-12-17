74b4d5f28eb8a56802bfc787928a03dc14cb8428
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-17 22:32:22 +0100

    Revert "feat: Install all parser for treesitter"
    This reverts commit f791ab8caf861c61f66f2340e65f05c1831636b9.

--- lua/fredrkl/plugins/nvim-treesitter.lua
+++ lua/fredrkl/plugins/nvim-treesitter.lua
@@ -8,7 +8,7 @@ return {
   build = ':TSUpdate',
   config = function()
     require'nvim-treesitter.configs'.setup {
-      ensure_installed = "all",
+      ensure_installed = 'bicep',
       highlight = {
         enable = true,
       },
