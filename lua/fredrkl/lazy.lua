0dae1d81a87bde42c803269e23293b17f5918566
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-19 12:55:24 +0100

    chore: cleanup packer leftovers
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/lazy.lua
+++ lua/fredrkl/lazy.lua
@@ -10,15 +10,4 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
--- Example using a list of specs with the default options
-
 require("lazy").setup("fredrkl.plugins")
-
---  use 'wbthomason/packer.nvim'
---  use {
---	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
---	  -- or                            , branch = '0.1.x',
---         requires = { {'nvim-lua/plenary.nvim'} }
---  }
---  use 'nvim-tree/nvim-tree.lua'
---  use 'mbbill/undotree':
