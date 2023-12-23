6d20b912a81e7d66e88e6b93cc6256c036ffe916
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-23 11:36:44 +0100

    feat: gitsign settings
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/plugins/gitsigns.lua
+++ lua/fredrkl/plugins/gitsigns.lua
@@ -9,6 +9,8 @@ return {
       changedelete = { text = "▎" },
       untracked = { text = "▎" },
     },
+    current_line_blame = true,
+    signcolumn = true,
     on_attach = function(buffer)
       local gs = package.loaded.gitsigns
 
