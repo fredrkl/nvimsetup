52794821cabec2c08429485a84b9e2bf92a8b2a4
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-26 10:12:35 +0100

    closes: 32
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/plugins/nvim-lualine.lua
+++ lua/fredrkl/plugins/nvim-lualine.lua
@@ -2,6 +2,15 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require("lualine").setup()
+    require("lualine").setup({
+      sections = {
+        lualine_c = { 
+          {
+            "filename",
+            path = 2,
+          }
+        }
+      }
+    })
   end
 }
