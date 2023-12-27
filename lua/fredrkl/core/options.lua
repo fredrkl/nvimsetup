0e7611350196a67aa128672d2ea3f3d9a0c247a3
Fredrik Klingenberg <fredrkl@gmail.com>
2023-12-27 19:08:16 +0100

    closes: #34
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/core/options.lua
+++ lua/fredrkl/core/options.lua
@@ -17,3 +17,9 @@ local keymap = vim.keymap
 -- Probably because mapleader is not yet set
 keymap.set("n", "<leader>nb", "<cmd>bprevious<CR>", { noremap = true })
 keymap.set("n", "<leader>nf", "<cmd>bnext<CR>", { noremap = true })
+
+-- Window navigation
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
