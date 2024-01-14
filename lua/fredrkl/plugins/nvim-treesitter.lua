86da8b400fdd281990199b20c1db203f28a2caa4
Fredrik Klingenberg <fredrkl@gmail.com>
2024-01-14 10:35:39 +0100

    feat: c# treesitter
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/plugins/nvim-treesitter.lua
+++ lua/fredrkl/plugins/nvim-treesitter.lua
@@ -8,7 +8,7 @@ return {
   build = ':TSUpdate',
   config = function()
     require'nvim-treesitter.configs'.setup {
-      ensure_installed = {"bicep", "terraform"},
+      ensure_installed = {"bicep", "terraform", "c_sharp"},
       highlight = {
         enable = true,
       },
