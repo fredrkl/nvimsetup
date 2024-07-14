97736f08983930a4e67be1a6217b43f6ff89b7c9
Fredrik Klingenberg <fredrkl@gmail.com>
2024-07-14 13:00:18 +0200

    ✨(undotree): Description
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/plugins/undotree.lua
+++ lua/fredrkl/plugins/undotree.lua
@@ -2,7 +2,7 @@ return {
   "mbbill/undotree",
   config = function()
     local keymap = vim.keymap
-    keymap.set('n', '<leader>uu', vim.cmd.UndotreeToggle)
-    keymap.set('n', '<leader>uf', vim.cmd.UndotreeFocus)
+    keymap.set('n', '<leader>uu', vim.cmd.UndotreeToggle, { desc = "Toggle undotree" })
+    keymap.set('n', '<leader>uf', vim.cmd.UndotreeFocus, { desc = "Focus on undotree" })
   end,
 }
