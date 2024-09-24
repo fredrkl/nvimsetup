2221a4b4e3c434af05ceba26d10ac422a54bde22
Fredrik Klingenberg <fredrkl@gmail.com>
2024-09-24 17:46:58 +0200

    ✨ Spelling suggestion keymapping
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/core/options.lua
+++ lua/fredrkl/core/options.lua
@@ -26,3 +26,5 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sp", "<cmd>set spell!<CR>", { desc = "Toggle spell checking" }) -- Toogle spellchecker
+keymap.set('n', '<leader>ss', "z=", { noremap = true, silent = true, desc = "Spelling suggestions" })
