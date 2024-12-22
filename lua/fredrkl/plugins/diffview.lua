bb212e71f3a3d8671287fb63ec4af334b256b8f7
Fredrik Klingenberg <fredrkl@gmail.com>
2024-12-22 11:50:33 +0100

    ✨ Diffview shortcuts

--- lua/fredrkl/plugins/diffview.lua
+++ lua/fredrkl/plugins/diffview.lua
@@ -1,3 +1,9 @@
 return {
-  'sindrets/diffview.nvim'
+  'sindrets/diffview.nvim',
+  config = function()
+    require('diffview').setup()
+    vim.api.nvim_set_keymap('n', '<leader>do', ':DiffviewOpen<CR>', { noremap = true, silent = true, desc = "Open Diffview" })
+    vim.api.nvim_set_keymap('n', '<leader>dc', ':DiffviewClose<CR>', { noremap = true, silent = true, desc = "Close Diffview" })
+    vim.api.nvim_set_keymap('n', '<leader>dr', ':DiffviewRefresh<CR>', { noremap = true, silent = true, desc = "Refresh Diffview" })
+  end,
 }
