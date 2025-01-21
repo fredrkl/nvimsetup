91bfe73cf8dbf46ac602c23f25912f95949da267
Fredrik Klingenberg <fredrkl@gmail.com>
2025-01-21 13:41:46 +0100

    ✨ File history shortcuts
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/plugins/diffview.lua
+++ lua/fredrkl/plugins/diffview.lua
@@ -5,5 +5,7 @@ return {
     vim.api.nvim_set_keymap('n', '<leader>do', ':DiffviewOpen<CR>', { noremap = true, silent = true, desc = "Open Diffview" })
     vim.api.nvim_set_keymap('n', '<leader>dc', ':DiffviewClose<CR>', { noremap = true, silent = true, desc = "Close Diffview" })
     vim.api.nvim_set_keymap('n', '<leader>dr', ':DiffviewRefresh<CR>', { noremap = true, silent = true, desc = "Refresh Diffview" })
+    vim.api.nvim_set_keymap('n', '<leader>dh', ':DiffviewFileHistory<CR>', { noremap = true, silent = true, desc = "File history Diffview" })
+    vim.api.nvim_set_keymap('n', '<leader>df', ':DiffviewFileHistory %<CR>', { noremap = true, silent = true, desc = "File history file Diffview" })
   end,
 }
