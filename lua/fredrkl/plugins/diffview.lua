b96f4d4c442efd3aaabb334d59b8d971197ebb1b
Fredrik Klingenberg <fredrkl@gmail.com>
2025-06-21 18:39:02 +0200

    ✨ Better diagnostic shortcuts

--- lua/fredrkl/plugins/diffview.lua
+++ lua/fredrkl/plugins/diffview.lua
@@ -2,10 +2,10 @@ return {
   'sindrets/diffview.nvim',
   config = function()
     require('diffview').setup()
-    vim.api.nvim_set_keymap('n', '<leader>do', ':DiffviewOpen<CR>', { noremap = true, silent = true, desc = "Open Diffview" })
-    vim.api.nvim_set_keymap('n', '<leader>dc', ':DiffviewClose<CR>', { noremap = true, silent = true, desc = "Close Diffview" })
-    vim.api.nvim_set_keymap('n', '<leader>dr', ':DiffviewRefresh<CR>', { noremap = true, silent = true, desc = "Refresh Diffview" })
-    vim.api.nvim_set_keymap('n', '<leader>dh', ':DiffviewFileHistory<CR>', { noremap = true, silent = true, desc = "File history Diffview" })
-    vim.api.nvim_set_keymap('n', '<leader>df', ':DiffviewFileHistory %<CR>', { noremap = true, silent = true, desc = "File history file Diffview" })
+    vim.api.nvim_set_keymap('n', '<leader>go', ':DiffviewOpen<CR>', { noremap = true, silent = true, desc = "Open Diffview" })
+    vim.api.nvim_set_keymap('n', '<leader>gc', ':DiffviewClose<CR>', { noremap = true, silent = true, desc = "Close Diffview" })
+    vim.api.nvim_set_keymap('n', '<leader>gr', ':DiffviewRefresh<CR>', { noremap = true, silent = true, desc = "Refresh Diffview" })
+    vim.api.nvim_set_keymap('n', '<leader>gh', ':DiffviewFileHistory<CR>', { noremap = true, silent = true, desc = "File history Diffview" })
+    vim.api.nvim_set_keymap('n', '<leader>gf', ':DiffviewFileHistory %<CR>', { noremap = true, silent = true, desc = "File history file Diffview" })
   end,
 }
