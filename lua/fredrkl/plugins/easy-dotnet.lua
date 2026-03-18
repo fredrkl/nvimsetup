751051f91efafdb7e91fda01093ad67cf384ed8c
Fredrik Klingenberg <fredrkl@gmail.com>
2026-03-18 07:41:55 +0100

    ✨ Easy dotnet debugging keymappings
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/plugins/easy-dotnet.lua
+++ lua/fredrkl/plugins/easy-dotnet.lua
@@ -15,5 +15,8 @@ return {
   config = function()
     require("easy-dotnet").setup()
     setup_global_keymaps()
+    vim.api.nvim_set_keymap('n', '<F5>', "<cmd>lua require'dap'.continue()<CR>", { noremap = true, silent = true, desc = "Start/Continue" })
+    vim.api.nvim_set_keymap('n', '<F7>', "<cmd>lua require'dap'.step_over()<CR>", { noremap = true, silent = true, desc = "Step Over" })
+    vim.api.nvim_set_keymap('n', '<F8>', "<cmd>lua require'dap'.step_into()<CR>", { noremap = true, silent = true, desc = "Step Into" })
   end
 }
