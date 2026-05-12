d865fc14de0884847f5ef3294054ddcc7dc04935
Fredrik Klingenberg <fredrkl@gmail.com>
2026-05-12 14:39:34 +0200

    ✨ VeryLazy load lualine
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lua/fredrkl/plugins/nvim-lualine.lua
+++ lua/fredrkl/plugins/nvim-lualine.lua
@@ -1,10 +1,11 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  event = "VeryLazy",
   config = function()
     require("lualine").setup({
       sections = {
-        lualine_c = { 
+        lualine_c = {
           {
             "filename",
             path = 1,
