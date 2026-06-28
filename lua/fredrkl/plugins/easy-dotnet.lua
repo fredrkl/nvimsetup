c3056bda7f3e7b34582223dc02e90d16330048f3
Fredrik Klingenberg <fredrkl@gmail.com>
2026-06-28 12:37:24 +0200

    ⚰️ Clenup easy-dotnet

--- lua/fredrkl/plugins/easy-dotnet.lua
+++ lua/fredrkl/plugins/easy-dotnet.lua
@@ -1,17 +1,6 @@
 return {
   "GustavEikaas/easy-dotnet.nvim",
   dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
---  opts = {
---     lsp = {
---       config = {
---         settings = {
---           ["csharp|code_lens"] = {
---             dotnet_enable_references_code_lens = true,
---           },
---         },
---       },
---     },
---  },
   config = function()
     require("easy-dotnet").setup()
     setup_global_keymaps()
